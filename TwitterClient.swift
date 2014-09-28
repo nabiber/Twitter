b1b4ed7beb46ca50810b49852dfc69d8099afb66
Nabib El-Rahman <nabib.el-rahman@dailymotion.com>
2014-09-28 15:35:41 -0700

    first commit

---
+++ TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  Twitter
+//
+//  Created by Nabib El-RAHMAN on 9/28/14.
+//  Copyright (c) 2014 Nabib El-RAHMAN. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: BDBOAuth1RequestOperationManager {
+   
+}
